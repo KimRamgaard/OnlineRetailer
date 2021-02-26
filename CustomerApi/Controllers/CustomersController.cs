c259f35de7d25064dbed789759d291aca0fedc01
Pierre Hübertz Knudsen <pierreknudsen123@gmail.com>
2021-02-26 12:19:40 +0100

    Added CustomerController with Post, Put, Get, Delete

---
+++ CustomerApi/Controllers/CustomersController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using CustomerApi.Data;
+using CustomerApi.Models;
+
+namespace CustomerApi.Controllers
+{
+    public class CustomersController : ControllerBase
+    {
+
+        private readonly IRepository<Customer> repository;
+
+        public CustomersController(IRepository<Customer> repos)
+        {
+            repository = repos;
+        }
+
+        // GET customer/id
+        [HttpGet("{id}", Name = "GetCustomer")]
+        public IActionResult Get(int id)
+        {
+            var customer = repository.Get(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(customer);
+        }
+
+        // POST customer
+        [HttpPost]
+        public IActionResult Post([FromBody] Customer customer)
+        {
+            if (customer == null)
+            {
+                return BadRequest("Seems like you didn't send any information");
+            }
+
+            var newCustomer = repository.Add(customer);
+
+            return CreatedAtRoute("GetCustomer", new { id = newCustomer.CustomerId }, newCustomer);
+        }
+
+        // PUT customer/id
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Customer customer)
+        {
+            if (customer == null || customer.CustomerId != id)
+            {
+                return BadRequest();
+            }
+
+            var modifiedCustomer = repository.Get(id);
+
+            if (modifiedCustomer == null)
+            {
+                return NotFound();
+            }
+
+            modifiedCustomer.FullName = customer.FullName;
+            modifiedCustomer.Email = customer.Email;
+            modifiedCustomer.Phone = customer.Phone;
+            modifiedCustomer.BillingAddress = customer.BillingAddress;
+            modifiedCustomer.ShippingAddress = customer.ShippingAddress;
+
+            repository.Edit(modifiedCustomer);
+            return new NoContentResult();
+        }
+
+        // DELETE customer/id
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            if (repository.Get(id) == null)
+            {
+                return NotFound();
+            }
+
+            repository.Remove(id);
+            return new NoContentResult();
+        }
+
+    }
+}
