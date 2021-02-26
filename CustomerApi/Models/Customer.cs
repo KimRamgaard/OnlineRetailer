470c63701480fadb445a23c15efea7a4a558fbcc
Pierre Hübertz Knudsen <pierreknudsen123@gmail.com>
2021-02-26 11:07:20 +0100

    Added customer api & customer model

---
+++ CustomerApi/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerApi.Models
+{
+    public class Customer
+    {
+        int CustomerId { get; set; }
+        string FullName { get; set; }
+        string Email { get; set; }
+        string Phone { get; set; }
+        string BillingAddress { get; set; }
+        string ShippingAddress { get; set; }
+        double CreditStanding { get; set; }
+
+    }
+}
