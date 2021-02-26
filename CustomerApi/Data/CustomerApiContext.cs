c1591f4daa9a565b6a502373d8fb5427c666d2f6
Pierre Hübertz Knudsen <pierreknudsen123@gmail.com>
2021-02-26 11:15:33 +0100

    Added Context

---
+++ CustomerApi/Data/CustomerApiContext.cs
@@ -0,0 +1,19 @@
+﻿using CustomerApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerApi.Data
+{
+    public class CustomerApiContext : DbContext
+    {
+        public CustomerApiContext(DbContextOptions<CustomerApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Customer> Products { get; set; }
+    }
+}
