65b406f6077ba4d89cbb700360e77fd49cb5cc69
Kim Ramgaard <kim.ramgaard@gmail.com>
2021-03-18 15:09:28 +0100

    Added Entity framework to shared model so Orderline can get a proper key

--- SharedModels/OrderLine.cs
+++ SharedModels/OrderLine.cs
@@ -1,12 +1,15 @@
-﻿
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
 namespace SharedModels
 {
-    public class OrderLine
-    {
-        public int ProductId { get; set; }
-        public int Quantity { get; set; }
 
+    [Keyless]
+    public class OrderLine
+    {  
         public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }       
 
     }
 }
